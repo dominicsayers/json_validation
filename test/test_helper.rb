2b9cb6ea1f276849a2886a0f0884e6dc93e8edbc
Peter Inglesby <peter.inglesby@gmail.com>
2016-04-11 23:03:24 +0100

    Start WEBrick server inside tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,20 @@ require 'mocha/mini_test'
 require 'json_validator'
 
 #Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
+
+def serve_test_data
+  require 'webrick'
+
+  document_root = File.join(File.dirname(__FILE__), 'json-schema-test-suite', 'remotes')
+
+  s = WEBrick::HTTPServer.new(
+    Port: 1234,
+    DocumentRoot: document_root,
+    Logger: WEBrick::Log.new(File::NULL),
+    AccessLog: [],
+  )
+
+  Thread.new {s.start}
+end
+
+serve_test_data
